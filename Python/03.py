f235f74002a498ce852467c0f5bdc69910d96c36
Fahlevi20 <rozakalfi@gmail.com>
2024-01-26 10:30:05 +0700

    fix 03.py

--- Python/03.py
+++ Python/03.py
@@ -1,7 +1,7 @@
 import numpy as np
-
-change_array = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9
-])
-
-reshape= np.reshape(change_array,(3,3))
+value = 1, 2, 3, 4, 5, 6, 7, 8, 9
+arr = np.array(list(map(int,input().split())))
+# change_array = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9
+# ])
+reshape = np.reshape(arr,(3,3))
 print(reshape)
